9924bcd479fead28c2f7a16d9cea608da90a9aa2
pvtfurkan <furki304@gmail.com>
2023-02-26 21:33:58 +0300

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.Models
+{
+    public class Category
+    {
+        [Key] //entity frmwrk
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100 only!!")]
+        public string DisplayOrder { get; set; }
+        public string CreatedDateTime { get; set; } = DateTime.Now.ToString();
+
+    }
+}
